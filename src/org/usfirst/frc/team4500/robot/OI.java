374245ddc9d03245eddb29aaed86ecaa0736778f
Tominator368 <thomas.schweich@gmail.com>
2016-02-13 13:29:46 -0600

    Moved the coprocessor stuff to its own class

--- src/org/usfirst/frc/team4500/robot/OI.java
+++ src/org/usfirst/frc/team4500/robot/OI.java
@@ -16,45 +16,15 @@ import edu.wpi.first.wpilibj.buttons.Button;
  */
 public class OI {
 	Joystick stick;
-	Socket coprocessorSock;
-	BufferedReader socketReader;
 	
 	/**
 	 * Initializes the joystick and the coprocessor socket;
 	 * The coprocesser socket may be a null socket - be warned.
 	 */
 	public OI() {
-		
 		stick = new Joystick(0);
-		initializeSocket();
-		
-		
 	}
 	
-	/**
-	 * Initializes coprocessor socket;
-	 * The coprocesser socket may be a null socket - be warned.
-	 */
-	public boolean initializeSocket(){
-		try {
-			coprocessorSock = new Socket(RobotMap.COPROCESSOR_ADDRESS, RobotMap.COPROCESSOR_PORT);
-			socketReader = new BufferedReader(new InputStreamReader(coprocessorSock.getInputStream()));
-			
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			coprocessorSock = null;
-			socketReader = null;
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			coprocessorSock = null;
-			socketReader = null;
-			return false;
-		}
-		
-		return true;
-	}
 	
 	/**
 	 * The value of the x axis of the joystick, adjusted for deadzones and any necessary scaling
@@ -80,33 +50,6 @@ public class OI {
 		return (stick.getTwist() < RobotMap.DEADZONE) ? 0 : stick.getTwist();
 	}
 	
-	public double getGoalAngle(){
-		/**
-		 * Gets the angle above the horizontal that the goal is.
-		 * The format of the reading is 8 bytes then \n
-		 */
-		
-		try {
-			coprocessorSock.getOutputStream().write(0); //Tell the coprocessor that we want an angle
-			String angleInString = socketReader.readLine();
-			
-			byte[] angleInBytes = angleInString.substring(0, angleInString.length() - 1).getBytes(); //skip the newline character
-			
-			return ByteBuffer.wrap(angleInBytes).getDouble();
-			
-			
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		return -1;
-		
-	}
-
-	public boolean socketInitialized() {
-		// TODO Auto-generated method stub
-		return !coprocessorSock.equals(null);
-	}
+	
 }
 
---
+++ src/utilities/VisionServer.java
@@ -0,0 +1,77 @@
+package utilities;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+
+import org.usfirst.frc.team4500.robot.RobotMap;
+
+public class VisionServer {
+	
+	
+	Socket coprocessorSock;
+	BufferedReader socketReader;
+	
+	public VisionServer() {
+		initializeSocket();
+	}
+	
+	
+	/**
+	 * Initializes coprocessor socket;
+	 * The coprocesser socket may be a null socket - be warned.
+	 */
+	public boolean initializeSocket(){
+		try {
+			coprocessorSock = new Socket(RobotMap.COPROCESSOR_ADDRESS, RobotMap.COPROCESSOR_PORT);
+			socketReader = new BufferedReader(new InputStreamReader(coprocessorSock.getInputStream()));
+			
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			coprocessorSock = null;
+			socketReader = null;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			coprocessorSock = null;
+			socketReader = null;
+			return false;
+		}
+		
+		return true;
+	}
+	
+	
+	/**
+	 * Gets the angle above the horizontal that the goal is.
+	 * The format of the reading is 8 bytes then \n
+	 */
+	public double getGoalAngle(){
+		try {
+			coprocessorSock.getOutputStream().write(0); //Tell the coprocessor that we want an angle
+			String angleInString = socketReader.readLine();
+			
+			byte[] angleInBytes = angleInString.substring(0, angleInString.length() - 1).getBytes(); //skip the newline character
+			
+			return ByteBuffer.wrap(angleInBytes).getDouble();
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return -1;
+		
+	}
+
+	public boolean socketInitialized() {
+		// TODO Auto-generated method stub
+		return !coprocessorSock.equals(null);
+	}
+
+}
