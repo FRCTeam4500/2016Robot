b957664716243398b4a000b319f8024ae352e872
Tominator368 <thomas.schweich@gmail.com>
2016-02-22 21:07:07 -0600

    Loading command (last commit was incomplete)

---
+++ src/org/usfirst/frc/team4500/robot/commands/MoveLoader.java
@@ -0,0 +1,42 @@
+package org.usfirst.frc.team4500.robot.commands;
+
+import org.usfirst.frc.team4500.robot.Robot;
+import org.usfirst.frc.team4500.robot.subsystems.Loader;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class MoveLoader extends Command {
+	
+	Loader.loadDirection direction;
+
+    public MoveLoader(Loader.loadDirection direction) {
+        requires(Robot.loader);
+        this.direction = direction;
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.loader.load(direction);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return true;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
