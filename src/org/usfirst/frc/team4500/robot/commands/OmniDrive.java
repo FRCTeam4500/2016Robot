b817dc5a28d154f4f5781e4a0e66918bbde19e5c
Tominator368 <thomas.schweich@gmail.com>
2016-03-01 17:09:07 -0600

    Omni drive modifications.

--- src/org/usfirst/frc/team4500/robot/commands/OmniDrive.java
+++ src/org/usfirst/frc/team4500/robot/commands/OmniDrive.java
@@ -21,10 +21,10 @@ public class OmniDrive extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.drivetrain.tankDrive(Robot.oi.getJoyY(), -Robot.oi.getJoyTwist());
+    	//Robot.drivetrain.tankDrive(Robot.oi.getJoyY(), -Robot.oi.getJoyTwist());
     	//TODO temp uses tankdrive
     	//Gives the omni drive functions the square of the joystick axes, making for smoother control.
-    	//Robot.drivetrain.omniDrive(Math.pow(Robot.oi.getJoyX(), 2), Math.pow(Robot.oi.getJoyY(), 2), Math.pow(Robot.oi.getJoyTwist(), 2), Robot.drivetrain.getGyroAngle());
+    	Robot.drivetrain.omniDrive(Math.pow(Robot.oi.getJoyX(), 2), Math.pow(Robot.oi.getJoyY(), 2), Math.pow(Robot.oi.getJoyTwist(), 2), Robot.drivetrain.getGyroAngle());
     }
 
     // Make this return true when this Command no longer needs to run execute()
